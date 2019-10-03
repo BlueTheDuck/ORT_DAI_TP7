60d85f27c5cdd57ef7d7fa3687600822b3d2a8ea
Android Studio <devandroid@ort>
2019-10-03 12:01:54 -0300

    SUPER MEGA FUNCTION THAT CALCULATES THE DISTANCE FROM THE SPRITE TO ANY OF THE 4 CORNERS

--- app/src/main/java/com/ducklings_corp/tp7/MainActivity.java
+++ app/src/main/java/com/ducklings_corp/tp7/MainActivity.java
@@ -11,7 +11,11 @@ import org.cocos2d.nodes.Director;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.types.CCPoint;
 import org.cocos2d.types.CCSize;
+
+
+
 
 public class MainActivity extends Activity {
 
@@ -27,44 +31,44 @@ public class MainActivity extends Activity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        MainView=new CCGLSurfaceView (this);
+        MainView = new CCGLSurfaceView(this);
         setContentView(MainView);
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         clsGame myGame;
-        myGame=new clsGame(MainView);
+        myGame = new clsGame(MainView);
         myGame.startGame();
     }
 
     public class clsGame {
         CCGLSurfaceView _GameView;
-        CCSize _Screen;
-        Sprite _Gaymer;
+        CCSize _screen;
+        Sprite _gaymer;
 
-        public clsGame (CCGLSurfaceView GameView){
+        public clsGame(CCGLSurfaceView GameView) {
             Log.d("clsGame", "clsgame");
-            _GameView=GameView;
+            _GameView = GameView;
         }
 
-        public void startGame(){
+        public void startGame() {
             Log.d("clsGame", "startgame");
 
             Director.sharedDirector().attachInView(_GameView);
 
-            _Screen=Director.sharedDirector().displaySize();
-            Log.d("clsGame", "Pantalla - Ancho: "+_Screen.getWidth()+" - Alto: "+_Screen.getHeight());
+            _screen = Director.sharedDirector().displaySize();
+            Log.d("clsGame", "Pantalla - Ancho: " + _screen.getWidth() + " - Alto: " + _screen.getHeight());
 
             Scene scene = startScene();
             Director.sharedDirector().runWithScene(scene);
         }
 
-        private Scene startScene(){
+        private Scene startScene() {
             Log.d("clsGame", "start");
             Scene returnScene;
-            returnScene=Scene.node();
+            returnScene = Scene.node();
 
             Log.d("clsGame", "startScene");
             GameLayer aLayer = new GameLayer();
@@ -74,9 +78,49 @@ public class MainActivity extends Activity {
         }
 
         class GameLayer extends Layer {
+            public GameLayer() {
+                place();
+            }
 
+            void place() {
+                _gaymer = Sprite.sprite("gaymer.jpg");
+                CCPoint initialPos = new CCPoint();
+                initialPos.x = (float)Math.random() * _screen.getWidth();
+                initialPos.y = (float)Math.random() * _screen.getHeight();
+                _gaymer.setPosition(initialPos.x,initialPos.y);
+
+                findCorner(initialPos);
+
+                super.addChild(_gaymer);
+            }
+        }
+        CCPoint findCorner(CCPoint point) {
+            int x = (int)point.x, y = (int)point.y;
+            CCPoint[] corners = {
+                    CCPoint.make(0f,_screen.height),
+                    CCPoint.make(_screen.width,_screen.height),
+                    CCPoint.make(0,  0),
+                    CCPoint.make(_screen.width,  0),
+            };
+            int farthest = -1;
+            float distance = 0;
+            for(int i = 0; i < 4;i++) {
+                float cornerX = corners[i].x,cornerY = corners[i].y;
+                float dst = pythagoras( cornerX-x ,cornerY-y  );
+                if(dst>distance) {
+                    distance = dst;
+                    farthest = i;
+                }
+            }
+            return corners[farthest];
         }
 
+    }
+
+
+
+    float pythagoras(float w,float h) {
+        return (float)Math.sqrt(w*w+h*h);
     }
 
 }
